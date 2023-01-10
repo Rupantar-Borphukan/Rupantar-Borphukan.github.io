84b48295ce8db1278cc60f3898b1cc198988bb45
Rupantar Borphukan <94108574+Rupantar-Borphukan@users.noreply.github.com>
2023-01-10 10:33:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Web Developer","Competitive Programmer" ,"Open Source Contributor","Blockchain Enthusiast"],
+        strings: ["MERN Stack Developer", "Open Source Contributor", "Blockchain Enthusiast"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Web Developer","Competitive Programmer","Open Source Contributor","Blockchain Enthusiast"],
+        strings: ["MERN Stack Developer","Open Source Contributor","Blockchain Enthusiast"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
